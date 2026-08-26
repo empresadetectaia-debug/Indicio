b9a51e84573c0ce753dce82df2841a732b9b1cfd
empresadetectaia-debug <empresa.detectaia@gmail.com>
2026-08-26 13:06:38 -0600

    Update layout.tsx

--- detector-ia/src/app/layout.tsx
+++ detector-ia/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           crossOrigin="anonymous"
         ></script>
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
